e7ca3e765dbec6ac378040c530d5ce954cc257f0
Rajdev-ullash <rajdevullash111@gmail.com>
2023-06-08 13:24:20 +0600

    [Done] Zod setup

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
-import express, { Application } from 'express'
-import cors from 'cors'
-const app: Application = express()
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/users/user.route'
+import express, { Application } from 'express';
+import cors from 'cors';
+const app: Application = express();
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/user/user.route';
 
-app.use(cors())
+app.use(cors());
 
 //parsers
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Application routes
 
-app.use('/api/v1/users/', UserRoutes)
+app.use('/api/v1/users/', UserRoutes);
 
 //global error handler
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 //test route
 
-export default app
+export default app;
