c350ada07d65f303795975129ffcc55087d3488b
Rajdev-ullash <rajdevullash111@gmail.com>
2023-06-21 20:54:54 +0600

    [Done] faculty, admin, managementDepartment module

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,8 +1,9 @@
 import httpStatus from 'http-status';
-import { Request, Response } from 'express';
+import { Request, RequestHandler, Response } from 'express';
 import catchAsync from '../../../shared/catchAsync';
 import { UserService } from './user.service';
 import sendResponse from '../../../shared/sendResponse';
+import { IUser } from './user.interface';
 const createStudent = catchAsync(async (req: Request, res: Response) => {
   const { student, ...user } = req.body;
   const result = await UserService.createStudent(student, user);
@@ -14,6 +15,36 @@ const createStudent = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const createFaculty: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { faculty, ...userData } = req.body;
+    const result = await UserService.createFaculty(faculty, userData);
+
+    sendResponse<IUser>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'user created successfully!',
+      data: result,
+    });
+  }
+);
+
+const createAdmin: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { admin, ...userData } = req.body;
+    const result = await UserService.createAdmin(admin, userData);
+
+    sendResponse<IUser>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Admin created successfully!',
+      data: result,
+    });
+  }
+);
+
 export const UserController = {
   createStudent,
+  createFaculty,
+  createAdmin,
 };
