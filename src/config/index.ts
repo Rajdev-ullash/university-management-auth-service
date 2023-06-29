13fba307d25da5bb0c12b447d14b1fcfc9653ffb
Rajdev-ullash <rajdevullash111@gmail.com>
2023-06-29 15:50:55 +0600

    [Done] authentication & authorization

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,4 +10,12 @@ export default {
   default_student_pass: process.env.DEFAULT_STUDENT_PASS,
   default_faculty_pass: process.env.DEFAULT_FACULTY_PASS,
   default_admin_pass: process.env.DEFAULT_ADMIN_PASS,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    refresh_secret: process.env.JWT_REFRESH_SECRET,
+    expires_in: process.env.JWT_EXPIRES_IN,
+    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+    bcrypt_salt_rounds: process.env.JWT_BCRYPT_SALT_ROUNDS,
+  },
 };
